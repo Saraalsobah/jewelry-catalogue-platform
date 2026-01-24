29cfc03c28392d192df350f9e7c23ae183db4962
Saraalsobah <salsobah2006@gmail.com>
2026-01-24 21:34:42 +0300

    Fixed some things in the views

--- controllers/jewelryPiece.routes.js
+++ controllers/jewelryPiece.routes.js
@@ -4,7 +4,7 @@ const User = require("../models/User")
 
 
 router.get('/', async (req, res) => {
-    const allPieces = await JewelryPiece.find() 
+    const allPieces = await JewelryPiece.find().populate('designer')
     res.render('jewelryPiece/all-jewelry.ejs', { allPieces: allPieces })
 })
 
@@ -14,13 +14,7 @@ router.get('/new', async(req,res)=>{
 })
 router.post('/new', async(req,res)=>{
     createdPiece = await JewelryPiece.create(req.body)
+    res.redirect('/jewelry')
 })
-
-router.get('/id', async (req,res)=>{
-
-})
-
-
-
 
 module.exports = router;
