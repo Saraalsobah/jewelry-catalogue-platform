19a93f9f429a12f240059d759efaa72c02d4c87f
Saraalsobah <salsobah2006@gmail.com>
2026-01-25 19:42:09 +0300

    Created update route + ejs page

--- controllers/jewelryPiece.routes.js
+++ controllers/jewelryPiece.routes.js
@@ -12,6 +12,7 @@ router.get('/new', async(req,res)=>{
     const allDesigners = await User.find()
     res.render('jewelryPiece/create-piece.ejs', {allDesigners:allDesigners})
 })
+
 router.post('/new', async(req,res)=>{
     createdPiece = await JewelryPiece.create(req.body)
     res.redirect('/jewelry')
@@ -19,7 +20,18 @@ router.post('/new', async(req,res)=>{
 
 router.get('/:id', async (req, res) => {
  const onePiece = await JewelryPiece.findById(req.params.id)
- res.render('jewelryPiece/jewelry-details.ejs', { onePiece: onePiece })
+ res.render('jewelryPiece/jewelry-details.ejs', { onePiece:onePiece })
+})
+
+router.get('/update/:id', async (req, res) => {
+ const onePiece = await JewelryPiece.findById(req.params.id)
+ const allDesigners = await User.find()
+ res.render('jewelryPiece/update-jewelry.ejs', { onePiece:onePiece, allDesigners:allDesigners })
+})
+
+router.post('/update/:id', async (req, res) => {
+ const updatedPiece = await JewelryPiece.findByIdAndUpdate(req.params.id, req.body)
+ res.redirect('/jewelry')
 })
 
 module.exports = router;
